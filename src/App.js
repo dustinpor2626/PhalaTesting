2d2fc7398e4404731d5486435d3389a2832b4526
Sourav Singh <66918899+dustinpor2626@users.noreply.github.com>
2023-07-26 00:38:29 +0530

    updated one

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { options, ApiPromise, WsProvider, Keyring } from "@polkadot/api";
+import { ApiPromise, WsProvider, Keyring } from "@polkadot/api";
 import { ContractPromise } from "@polkadot/api-contract";
 import {
+  options,
   OnChainRegistry,
   signCertificate,
   PinkContractPromise,
